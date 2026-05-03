c3f828819ee0c94f0fb38d74877fe04c635f4fb2
Marc Lehmann <marc@navisense.de>
2026-05-03 07:25:30 +0200

    Fix license blurb wrapping

--- gateway/src/message.py
+++ gateway/src/message.py
@@ -12,8 +12,8 @@
 # A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
 # details.
 #
-# You should have received a copy of the GNU Affero General Public License along
-# with clawp. If not, see <https://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU Affero General Public License
+# along with clawp. If not, see <https://www.gnu.org/licenses/>.
 
 import abc
 import asyncio
