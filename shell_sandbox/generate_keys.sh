14c60fe456f9437a392ea581cac863e61063e49c
Marc Lehmann <marc@navisense.de>
2026-06-19 11:30:45 +0200

    Add shell sandbox container
    Add an ubuntu sidecar container that will act as a sandbox to execute
    agent shell commands in. A script run in an init container generates the
    key pair.

---
+++ shell_sandbox/generate_keys.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+# Copyright 2026 Marc Lehmann
+#
+# This file is part of clawp.
+#
+# clawp is free software: you can redistribute it and/or modify it under the
+# terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# clawp is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with clawp. If not, see <https://www.gnu.org/licenses/>.
+
+# This script generates an SSH key pair named id_shell_sandbox{,.pub} and places
+# it in /shared_ssh. It sets the owner of the private key to 1000:1000, which is
+# the user and group used inside the gateway container. It also copies the
+# public key to the filed authorized_keys in the same directory, so it can be
+# mounted directly.
+
+set -e
+
+KEY_DIR="/shared_ssh"
+PRIVATE_KEY="$KEY_DIR/id_shell_sandbox"
+PUBLIC_KEY="$KEY_DIR/id_shell_sandbox.pub"
+
+echo "Generating fresh SSH key pair for shell sandbox."
+yes | ssh-keygen -t ed25519 -N "" -f "$PRIVATE_KEY" -q -C "auto-generated key"
+chmod 600 "$PRIVATE_KEY"
+# Set private key owner to 1000:1000, which is the user ID in the gateway.
+chown 1000:1000 "$PRIVATE_KEY"
+
+echo "Preparing authorized_keys file."
+cp "$PUBLIC_KEY" "$KEY_DIR/authorized_keys"
+chmod 600 "$KEY_DIR/authorized_keys"
+
+echo "SSH keys initialized successfully."
